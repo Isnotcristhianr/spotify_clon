762a4dcc6092263a5dd4c3788857a0b444d75626
Cristhian David Recalde <crecalde@neaternotes.com>
2024-12-09 19:34:36 -0500

    Refactor CardPlayButton and Player components for improved audio handling and UI responsiveness; integrate dynamic playlist fetching and enhance button state management. Update PlayListCard to utilize CardPlayButton.

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,17 +1,39 @@
 import { Play, Pause } from "@/components/player";
 import { usePlayerStore } from "@/store/playerStore";
 
-export function CardPlayButton({ id}) {
-  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state);
+export function CardPlayButton({ id }) {
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
+    usePlayerStore((state) => state);
+
+  const isPlayingThisSong = isPlaying && currentMusic.playlistId === id;
 
   const handleClick = () => {
-    setCurrentMusic({ playlistId: id, songId: null, songs: [] });
-    setIsPlaying(!isPlaying);
-  }
+    if (isPlayingThisSong) {
+      setIsPlaying(false);
+      return;
+    }
+
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { songs } = data;
+        const firstSong = songs[0];
+        
+        setCurrentMusic({ 
+          playlistId: id,
+          songId: firstSong.id,
+          songs: songs
+        });
+        setIsPlaying(true);
+      });
+  };
 
   return (
-    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-2">
-      {isPlaying ? <Pause /> : <Play />}
+    <button
+      onClick={handleClick}
+      className="card-play-button rounded-full bg-green-500 p-2"
+    >
+      {isPlayingThisSong ? <Pause /> : <Play />}
     </button>
-  )
+  );
 }
--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,48 +1,39 @@
 import { usePlayerStore } from "@/store/playerStore";
 import { useState, useRef, useEffect } from "react";
 
-export const Pause = () => {
-  return (
-    <svg
-      role="img"
-      aria-hidden="true"
-      viewBox="0 0 16 16"
-      height="16"
-      fill="black"
-    >
+export const Pause = () => (
+    <svg role="img" aria-hidden="true" viewBox="0 0 16 16" height="16" fill="black">
       <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
     </svg>
-  );
-};
+);
 
-export const Play = () => {
-  return (
-    <svg
-      role="img"
-      aria-hidden="true"
-      viewBox="0 0 24 24"
-      height="16"
-      fill="black"
-    >
+export const Play = () => (
+    <svg role="img" aria-hidden="true" viewBox="0 0 24 24" height="16" fill="black">
       <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
     </svg>
-  );
-};
+);
 
 export function Player() {
-  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state);
+  const { isPlaying, setIsPlaying, currentMusic } = usePlayerStore(state => state);
   const audioRef = useRef();
 
   useEffect(() => {
-    audioRef.current.src = "/music/1/01.mp3";
-  }, []);
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const { playlistId, songId } = currentMusic;
+    if (playlistId && songId) {
+      const src = `/music/${playlistId}/0${songId}.mp3`;
+      audioRef.current.src = src;
+      audioRef.current.volume = 0.1;
+      audioRef.current.play()
+        .then(() => setIsPlaying(true))
+        .catch(e => console.error("Error playing audio:", e));
+    }
+  }, [currentMusic]);
 
   const handleClick = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-    }
     setIsPlaying(!isPlaying);
   };
 
@@ -51,10 +42,7 @@ export function Player() {
       <div>current song</div>
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
-          <button
-            className="bg-white rounded-full p-2"
-            onClick={handleClick}
-          >
+          <button className="bg-white rounded-full p-2" onClick={handleClick}>
             {isPlaying ? <Pause /> : <Play />}
           </button>
         </div>
