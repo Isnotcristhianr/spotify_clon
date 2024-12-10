2237aa2fc4e9c0266ace089fcdf4a4b9c6a6fd56
Cristhian David Recalde <crecalde@neaternotes.com>
2024-12-09 20:04:45 -0500

    Enhance Astro configuration and update dependencies; add slider component for volume control in Player. Introduce path aliasing for easier imports and update package-lock.json with new packages including Radix UI components and Tailwind CSS improvements.

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,12 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
+  output: 'server',
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src"
+      }
+    }
+  }
 });
--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,5 +1,7 @@
 import { usePlayerStore } from "@/store/playerStore";
 import { useState, useRef, useEffect } from "react";
+import { Slider } from "@/components/Slider";
+
 
 export const Pause = () => (
     <svg role="img" aria-hidden="true" viewBox="0 0 16 16" height="16" fill="black">
@@ -12,6 +14,20 @@ export const Play = () => (
       <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
     </svg>
 );
+
+const CurrentSong = ({ image, title, artists }) => {
+  return (
+    <div className="flex flex-row gap-5 overflow-hidden">
+      <picture className="w-16 h-16">
+        <img src={image} alt={title} className="object-cover w-full h-full rounded-md" />
+      </picture>
+      <div className="flex flex-col gap-1">
+        <h3 className="text-white text-sm font-bold">{title}</h3>
+        <span className="text-white text-xs font-bold opacity-50">{artists ? artists.join(', ') : ''}</span>
+      </div>
+    </div>
+  )
+}
 
 export function Player() {
   const { isPlaying, setIsPlaying, currentMusic } = usePlayerStore(state => state);
@@ -39,7 +55,7 @@ export function Player() {
 
   return (
     <div className="flex flex-row justify-between w-full px-4 z-50">
-      <div>current song</div>
+      <CurrentSong {...currentMusic.songs[0]} />
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
           <button className="bg-white rounded-full p-2" onClick={handleClick}>
@@ -49,6 +65,18 @@ export function Player() {
       </div>
       <div>
         <audio ref={audioRef}></audio>
+      </div>
+      <div className="grid place-content-center">
+        <Slider 
+          defaultValue={[100]}
+          max={100}
+          min={0}
+          step={1}
+          className="w-[95px] h-[5px] rounded-full"
+          onValueChange={(value) => {
+            audioRef.current.volume = value[0] / 100;
+          }}
+        />
       </div>
     </div>
   );
